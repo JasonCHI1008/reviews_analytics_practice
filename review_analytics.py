d7c3a3b8088082f9ad924c898930e3794badc224
CHI Jason <jason921008@gmail.com>
2023-04-27 15:48:52 +0900

    add word count function

--- review_analytics.py
+++ review_analytics.py
@@ -7,8 +7,49 @@ with open('reviews.txt', 'r') as f:
         if count % 1000 == 0:
             print(len(data))            
 print(len(data))
-sum_len = 0
+#word count------------------
+wc = {} #dictionary
 for d in data:
-    sum_len += len(d)
-ave = sum_len / 1000000
-print("the average of commment is", ave)
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1 #add new word in wc
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+print(len(wc))
+
+while True:
+    word = input("what word are you looking for:")
+    if word == "q":
+        break
+    if word in wc:
+        print(word, " appears", wc[word], "times")
+    else:
+        print('This word does not exist')
+
+
+
+
+
+
+
+
+
+
+
+# sum_len = 0
+# for d in data:
+#     sum_len += len(d)
+# ave = sum_len / 1000000
+# print("the average of commment is", ave)
+
+# new = []
+# for d in data:
+#     if len(d) < 100:
+#         new.append(d)
+# print('There are', len(new),'smaller than 100')
